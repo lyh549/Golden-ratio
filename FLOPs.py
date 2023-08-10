8324e4d0bca6252b08132f9883a26e00c5e51b36
lyh549 <857713037@qq.com>
2023-08-10 10:36:39 +0800

    Update FLOPs.py

--- FLOPs.py
+++ FLOPs.py
@@ -17,10 +17,8 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
     parser.add_argument('--dataset', type=str, default='cifar10',
                     help='training dataset (default: cifar10)')
-    parser.add_argument('--refine', default='', type=str, metavar='PATH',
-                        help='path to the pruned model to be fine tuned')
-    parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                        help='path to the pruned model to be fine tuned')
+    parser.add_argument('--pruned_model', default='', type=str, metavar='PATH',
+                        help='path to the pruned model')
     parser.add_argument('--arch', default='resnet', type=str,
                         help='architecture to use')
     parser.add_argument('--depth', default=164, type=int,
@@ -28,26 +26,13 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    if args.refine:
-        checkpoint = torch.load(args.refine)
+    if args.pruned_model:
+        checkpoint = torch.load(args.pruned_model)
         model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth, cfg=checkpoint['cfg'])
         model.load_state_dict(checkpoint['state_dict'])
     else:
         model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth)
     
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {}) Prec1: {:f}"
-                  .format(args.resume, checkpoint['epoch'], best_prec1))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
-
     num_parameters = sum([param.nelement() for param in model.parameters()])
     print("Number of parameter: %.2fM" % (num_parameters/1e6))
     input = torch.randn(1, 3, 32, 32)
